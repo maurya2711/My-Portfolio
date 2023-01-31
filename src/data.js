394e5514d4f45783920e851a05eb171c0606bac4
sharad_mourya <sharadmourya27@gmail.com>
2023-01-31 15:09:40 +0530

    my portfolio

---
+++ src/data.js
@@ -0,0 +1,48 @@
+import otaku from './assets/images/otaku.gif'
+import netflix from './assets/images/netflix.gif'
+export const projects = [
+  {
+    title: "The Otaku Point",
+    subtitle: "MENN Stack",
+    description:
+      "A Book/Manga/Novel reading platform for book lovers. Users have ability to read, update , download and add his favorites book/Manga/Novel",
+    image: otaku,
+    link: "https://otaku-point-bblictg7y-maurya2711.vercel.app/login",
+  },
+  {
+    title: "Netflix Clone",
+    subtitle: "React, Css, Bootstrap and Firebase",
+    description:
+      "This is a simple clone of Netflix Home page. It displays the list of movies, web series, Tv series. This also has trending section of movies from TMDB Api",
+    image: netflix,
+    link: "https://netflix-clone-b3df5.web.app/",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Node",
+  "Vue Js",
+  "Nuxt js",
+  "Micro-Frontend",
+  "Material UI",
+  "Bootstrap",
+];
